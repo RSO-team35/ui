10b8607c7e297e31ff34669a622ad4cb2fd2a0d7
jk2168 <jk2168@student.uni-lj.si>
2023-01-03 21:22:11 +0100

    added plots

--- ui_app/dashapp.py
+++ ui_app/dashapp.py
@@ -1,12 +1,26 @@
 import dash
 from dash.dependencies import Input, Output
-from dash import dcc, html
+from dash import dcc, html, dash_table
 import plotly.express as px
+import plotly.graph_objects as go
 import flask
 import pandas as pd
 import os
 import datetime
 import json 
+import random 
+import numpy as np
+
+
+def create_df(num=20, p=300):
+    df = pd.DataFrame({
+                    "retailer": [random.choice(["Mimovrste", "Amazon", "Microcenter"]) for _ in range(num)],
+                    "model": np.resize(["Dual OC", "Dual", "TUF GAming", "", "ROG Strix"], num),
+                    "manufacturer": np.resize(["ASUS", "Zotac"], num),
+                    "price": [round(p + random.uniform(-p*0.1, p*0.1), 2) for _ in range(num)],
+                    "date": pd.date_range(datetime.datetime.today(), periods=num).tolist()
+                })
+    return df
 
 
 def create_dash_app(requests_pathname_prefix: str = None) -> dash.Dash:
@@ -14,22 +28,9 @@ def create_dash_app(requests_pathname_prefix: str = None) -> dash.Dash:
     Sample Dash application from Plotly: https://github.com/plotly/dash-hello-world/blob/master/app.py
     """
 
-    dfs = {"GeForce RTX 3060": 
-                pd.DataFrame({
-                    "retailer": ["Mimovrste", "Mimovrste", "Amazon", "Amazon"],
-                    "model": ["Dual OC", "Dual", "", ""],
-                    "manufacturer": ["ASUS", "Zotac", "ASUS", "Gigabyte"],
-                    "price": [300, 250, 288, 320],
-                    "date": [datetime.date(2022, 12, 1), datetime.date(2022, 12, 2), datetime.date(2022, 12, 4), datetime.date(2022, 12, 8)]
-                }),
-            "GeForce RTX 3090":
-                pd.DataFrame({
-                    "retailer": ["Mimovrste", "Mimovrste", "Amazon", "Amazon"],
-                    "model": ["Dual OC", "Dual", "", ""],
-                    "manufacturer": ["ASUS", "Zotac", "ASUS", "Gigabyte"],
-                    "price": [1300, 1250, 1288, 1520],
-                    "date": [datetime.date(2022, 12, 1), datetime.date(2022, 12, 2), datetime.date(2022, 12, 4), datetime.date(2022, 12, 8)],
-                }),
+    
+    dfs = {"GeForce RTX 3060": create_df(20, 300),
+            "GeForce RTX 3090": create_df(20, 1500)
         }
 
     styles = {
@@ -39,7 +40,8 @@ def create_dash_app(requests_pathname_prefix: str = None) -> dash.Dash:
         }
     }
 
-    app = dash.Dash(__name__, requests_pathname_prefix=requests_pathname_prefix)
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, requests_pathname_prefix=requests_pathname_prefix, external_stylesheets=external_stylesheets)
 
     app.scripts.config.serve_locally = False
     dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
@@ -47,25 +49,80 @@ def create_dash_app(requests_pathname_prefix: str = None) -> dash.Dash:
     #fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
     app.layout = html.Div([
-        html.H1('Primerjalnik cen'),
+        html.H1('GPU price comparison'),
 
+        html.H4("GPU model selector"),
         dcc.Dropdown(
             id="price-history-dropdown",
             options=[{"label": n, "value": n} for n in list(dfs.keys())],
             value=list(dfs.keys())[0]
         ),
+
+        html.Div(className="row", children=[
+            
+            html.Div(className="one-half column", children=[
+                html.H4("Minimal price in recorded history"),
+                html.Div("Marked dot on graph", className="row"),
+                html.Pre(id="hover-data-2", style=styles["pre"])
+            ]),
+
+            html.Div(className="one-half column", children=[
+                html.H4("Currently selected details"),
+                html.Div("Hover over point on graph", className="row"),
+                html.Pre(id="hover-data", style=styles["pre"])
+            ]),
+        ], style={"margin-top": "30px"}),
+
         dcc.Graph(id='price-history'),
 
         html.Div(className="row", children=[
-            html.Div(className="column", children=[
-                dcc.Markdown("""
-                    **Details**
-                """),
-                html.Pre(id="hover-data", style=styles["pre"])
-            ]),
-        ])
+            html.H4(children="Prices per store with details"),
+            html.Div(id="table-comparison", style={"text-align":"center"})
+        ], style={"text-align":"center", "margin-top": "60px"}),
+
+        html.Div(className="row", children=[
+            html.H4(children="Lowest prices per store"),
+            dcc.Checklist(id="retailers-shown", inline=True),
+            dcc.Graph(id='price-history-retailer'),
+        ], style={"text-align":"center", "margin-top": "60px", "margin-bottom": "100px"}),
+        
 
     ], className="container")
+
+
+    @app.callback(
+        Output("table-comparison", "children"),
+        Input("price-history-dropdown", "value"))
+    def display_table(value):
+        df = dfs[value]
+
+        # get last price
+        last_pr = df[df.groupby("retailer")["date"].transform(max) == df["date"]]
+        last_pr = last_pr[["price", "date", "retailer"]]
+        last_pr.columns = ["last price", "last date", "retailer"]
+        #last_pr.loc["last date"] = last_pr["last date"].apply(lambda x: x.date())
+        print(last_pr)
+
+        agg_df = df[df.groupby("retailer")["price"].transform(min) == df["price"]]
+        #agg_df.loc["date"] = agg_df["date"].apply(lambda x: x.date())
+
+        new_df = pd.merge(agg_df, last_pr, on="retailer")
+        new_df["date"] = new_df["date"].apply(lambda x: x.date())
+        new_df["last date"] = new_df["last date"].apply(lambda x: x.date())
+        new_df = new_df.sort_values("price")
+        print(new_df)
+        return dash_table.DataTable(
+            data=new_df.to_dict("records"), 
+            columns=[{"id": c, "name": "Minimal price [EUR]" if c == "price" else ("Last recorded price [EUR]" if c == "last price" else c.capitalize())} for c in new_df.columns], 
+            style_cell={"textAlign":"left"},
+            style_cell_conditional=[{"if": {
+                "row_index": 0,
+                "column_id": "price"
+            },
+            "backgroundColor": "lightgreen",
+            }]
+        )
+
 
     @app.callback(
         Output("hover-data", "children"),
@@ -76,18 +133,77 @@ def create_dash_app(requests_pathname_prefix: str = None) -> dash.Dash:
 
         data = hoverData["points"][0]["customdata"]
         m = dcc.Markdown(f"""
-            ## {data[2]} {value} {data[1]}
+            ###### {data[2]} {value} {data[1]}
             - Retailer: {data[0]}
-            - Price: {hoverData["points"][0]["y"]:.2f}€
+            - Price: **{hoverData["points"][0]["y"]:.2f}€**
             - Date: {hoverData["points"][0]["x"]}
         """)
         return m
 
 
-    @app.callback(Output("price-history", "figure"), [Input("price-history-dropdown", "value")])
+    @app.callback(
+        Output("hover-data-2", "children"),
+        Input("price-history-dropdown", "value"))
+    def display_min_price(value):
+        df = dfs[value]
+
+        data = df.iloc[np.argmin(df["price"])]
+        m = dcc.Markdown(f"""
+            ###### {data["manufacturer"]} {value} {data["model"]}
+            - Retailer: {data["retailer"]}
+            - Price: **{data["price"]:.2f}€**
+            - Date: {data["date"]}
+        """) # todo add links?
+        return m
+
+
+    @app.callback(
+        Output("price-history", "figure"), 
+        [Input("price-history-dropdown", "value")])
     def update_graph(selected_dropdown_value):
         df = dfs[selected_dropdown_value]
-        fig = px.line(df, x="date", y="price", hover_data=df)
+        min_price = df.iloc[np.argmin(df["price"])]
+
+        fig = px.scatter(df, x="date", y="price", hover_data=df, labels={"date": "Date", "price":"Price"}, range_y=[min_price["price"]*0.9, max(df["price"])*1.05], trendline="lowess")
+        fig.update_yaxes(tickprefix="€")
+        #fig.update_layout(yaxis=dict(tickmode="linear", tick0=0, dtick=100))
+        #print(np.argmin(df["price"]))
+        
+        print(min_price)
+        fig.add_traces(go.Scatter(x=[min_price["date"]], y=[min_price["price"]], mode="markers", name="Minimal price", hoverinfo="skip"))
+        fig.update_traces(marker=dict(size=12, color="lightgreen"), selector=dict(mode="markers", name="Minimal price"))
+        #fig.update_layout(legend=dict(yanchor="bottom", y=0.05, xanchor="left", x=0.01))
+        return fig
+
+
+    @app.callback(
+        Output("retailers-shown", "options"), Output("retailers-shown", "value"),
+        Input("price-history-dropdown", "value"))
+    def hide_retailers(value):
+        df = dfs[value]
+        opt = df["retailer"].unique()
+        return (opt, opt)
+
+
+    @app.callback(
+        Output("price-history-retailer", "figure"), 
+        Input("price-history-dropdown", "value"), Input("retailers-shown", "value"))
+    def update_graph_retailer(value, display_val):
+        df = dfs[value]
+        #print(display_val)
+        df = df[df.retailer.isin(display_val)]
+
+        min_price = df.iloc[np.argmin(df["price"])]
+
+        fig = px.line(df, x="date", y="price", color="retailer", hover_data=df, markers=True, labels={"date": "Date", "price":"Price", "retailer": "Retailer"}, range_y=[min_price["price"]*0.9, max(df["price"])*1.05])
+        fig.update_yaxes(tickprefix="€")
+        #fig.update_layout(yaxis=dict(tickmode="linear", tick0=0, dtick=100))
+        #print(np.argmin(df["price"]))
+        
+        #print(min_price)
+        fig.add_traces(go.Scatter(x=[min_price["date"]], y=[min_price["price"]], mode="markers", name="Minimal price", hoverinfo="skip"))
+        fig.update_traces(marker=dict(size=12, color="lightgreen"), selector=dict(mode="markers"))
+        #fig.update_layout(legend=dict(yanchor="middle"))
         return fig
 
 
